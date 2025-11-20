df62e6e9f60fe4d4d00e9927cd1ed5154cbce6f8
behzod07-abdullayev <bekzod2366@gmail.com>
2025-11-20 12:12:18 +0500

    4-oy-3-dars

---
+++ main.js
@@ -0,0 +1,43 @@
+// 1-masala
+
+// const path = require("path")
+// const fullPath = path.join("/home", "user", "documents", "notes.txt" )
+// console.log(fullPath);
+
+
+
+// 2-masala
+
+// const path = require("path")
+// const files = ["document.pdf", "photo.jpeg", "archive.zip"]
+// for (let i = 0; i < files.length; i++) {
+// console.log(path.extname(files[i]));
+// }
+
+
+
+// 3-masala
+
+// const path = require ("path")
+// const file = ("D:/movies/horror/movie.mkv")
+// const newFile = "E:" + file.slice(2)
+// console.log(newFile);
+
+
+
+// 4-masala
+
+// const os = require('os');
+// const username = os.userInfo().username;
+// const homeDirectory = os.userInfo().homedir;
+// console.log(`Foydalanuvchi: ${username}`);
+// console.log(`Home katalogi: ${homeDirectory}`);
+
+
+
+// 5-masala
+
+// const os = require("os");
+// const interfaces = os.networkInterfaces();
+// console.log(interfaces);
+
